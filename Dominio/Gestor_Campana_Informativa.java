e9051f24873c2113b611f947335dffa2851e8dc6
almugcgm24 <91485233+almugcgm24@users.noreply.github.com>
2021-12-09 18:45:47 +0100

    update dominio

---
+++ Dominio/Gestor_Campana_Informativa.java
@@ -0,0 +1,8 @@
+package Dominio;
+
+public abstract class Gestor_Campana_Informativa {
+
+	public boolean registrarCampañas(Gestion_de_vacunacion aCampana_informativa) {
+		throw new UnsupportedOperationException();
+	}
+}
