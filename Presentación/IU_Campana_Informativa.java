9b8ba1d9f88cdc629d9de32badf70f81bb90f13c
David15556 <David.Arias3@alu.uclm.es>
2021-12-09 20:39:06 +0100

    maven

--- Presentación/IU_Campana_Informativa.java
+++ Presentación/IU_Campana_Informativa.java
@@ -1,4 +1,4 @@
-package PresentaciÃ³n;
+package Presentación;
 
 public class IU_Campana_Informativa extends IU_Administrador {
 }
--- Presentación/IU_Enfermedad.java
+++ Presentación/IU_Enfermedad.java
@@ -1,4 +1,4 @@
-package PresentaciÃ³n;
+package Presentación;
 
 public class IU_Enfermedad extends IU_Medico {
 }
