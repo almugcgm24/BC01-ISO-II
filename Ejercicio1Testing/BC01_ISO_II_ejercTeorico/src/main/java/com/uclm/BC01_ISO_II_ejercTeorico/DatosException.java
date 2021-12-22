0e25c8c0a6f75c6fe40c1b077506a1e68c46ff03
ams-cmd <72606010+ams-cmd@users.noreply.github.com>
2021-12-22 23:05:37 +0100

    madre mia

---
+++ Ejercicio1Testing/BC01_ISO_II_ejercTeorico/src/main/java/com/uclm/BC01_ISO_II_ejercTeorico/DatosException.java
@@ -0,0 +1,16 @@
+package BC01_ISO_II_ejercTeorico.src.main.java.com.uclm.BC01_ISO_II_ejercTeorico;
+
+public class DatosException extends Exception{
+	
+	public DatosException() { 
+	}
+	
+	
+	public DatosException(String mensaje) {
+		
+		super(mensaje); 
+	} 
+	
+	
+
+}
---
+++ Ejercicio1Testing/BC01_ISO_II_ejercTeorico/src/main/java/com/uclm/BC01_ISO_II_ejercTeorico/HumedadException.java
@@ -0,0 +1,16 @@
+package BC01_ISO_II_ejercTeorico.src.main.java.com.uclm.BC01_ISO_II_ejercTeorico;
+
+public class HumedadException extends Exception{
+	
+	public HumedadException() { 
+	}
+	
+	
+	public HumedadException(String mensaje) {
+		
+		super(mensaje); 
+	} 
+	
+	
+
+}
