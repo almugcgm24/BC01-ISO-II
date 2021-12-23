10f2d0f68992468e93643f733bae6aa24f63099e
ams-cmd <72606010+ams-cmd@users.noreply.github.com>
2021-12-23 18:16:37 +0100

    arregaldo?

--- src/main/java/com/uclm/BC01_ISO_II_ejercTeorico2/EdadException.java
+++
@@ -1,7 +0,0 @@
-package com.uclm.BC01_ISO_II_ejercTeorico2;
-
-public class EdadException extends Exception {
-    public EdadException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/uclm/BC01_ISO_II_ejercTeorico2/NoHayPlazasException.java
+++
@@ -1,6 +0,0 @@
-package com.uclm.BC01_ISO_II_ejercTeorico2;
-public class NoHayPlazasException extends Exception {
-    public NoHayPlazasException(String msg) {
-        super(msg);
-    }
-}
