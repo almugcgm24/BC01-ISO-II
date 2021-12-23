2456e1d951edffa4f3560a3d10438a422d12c453
Ana Martín <72606010+ams-cmd@users.noreply.github.com>
2021-12-23 20:47:22 +0100

    Delete Ejercicio2Testing directory

--- Ejercicio2Testing/src/main/java/com/uclm/BC01_ISO_II_ejercTeorico2/EdadException.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.com.uclm.BC01_ISO_II_ejercTeorico2;
-
-public class EdadException extends Exception {
-    public EdadException(String msg) {
-        super(msg);
-    }
-}
--- Ejercicio2Testing/src/main/java/com/uclm/BC01_ISO_II_ejercTeorico2/NegativoException.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.com.uclm.BC01_ISO_II_ejercTeorico2;
-public class NegativoException extends Exception {
-    public NegativoException(String msg) {
-        super(msg);
-    }
-}
